029f4c3beb6d45a498d2654861727558d5c4036e
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-26 14:20:32 -0300

    refactor(MenuType): change showMenu to static method

--- src/model/MenuType.java
+++ src/model/MenuType.java
@@ -16,7 +16,7 @@ public enum MenuType {
         return description;
     }
 
-    public void showMenu() {
+    public static void showMenu() {
         System.out.println("\n=== Sistema de Gerenciamento de Vídeos ===");
         for (MenuType menu : MenuType.values()) {
             System.out.println(menu.ordinal() + 1 + ". " + menu.getDescription());
