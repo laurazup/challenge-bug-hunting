4081e80fc980d36f82913b2ae2b5f46fc268561d
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-27 15:37:01 -0300

    docs(VideoService): add To-Do List

--- src/service/VideoService.java
+++ src/service/VideoService.java
@@ -5,6 +5,12 @@ import repository.VideoRepository;
 
 import java.util.List;
 
+// - Associar add com Crate
+// - Associar list com Read
+// - Associar alter com Update
+// - Associar erase com Delete
+// - Associar relate com Report
+
 public class VideoService {
     private final VideoRepository repository;
 
