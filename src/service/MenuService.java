0ca6eee4c23304f00b852e80d39359866ecf1923
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-27 10:46:42 -0300

    feat(MenuService): create return of interact method

--- src/service/MenuService.java
+++ src/service/MenuService.java
@@ -1,8 +1,30 @@
 package service;
 
+import model.MenuType;
+
+import java.util.Scanner;
+
 public class MenuService {
+    private static final int rangeOfOptions = MenuType.values().length;
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static boolean interact() {
-        return false;
+        boolean isOutsideRange;
+        MenuType.showMenu();
+        int chosenOption;
+        do {
+            System.out.print("Escolha uma opção entre 1 e" + rangeOfOptions + ": ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("A entrada digitada não é um número");
+                scanner.next();
+            }
+            chosenOption = scanner.nextInt();
+            isOutsideRange = chosenOption <= 0 || chosenOption > rangeOfOptions;
+            if (isOutsideRange) {
+                System.out.println("O número digitado está fora da faixa de valores.");
+            }
+        } while (isOutsideRange);
+
+        return chosenOption != MenuType.EXIT.ordinal() + 1;
     }
 }
