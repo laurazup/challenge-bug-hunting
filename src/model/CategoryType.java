a29abc2a61d3e5821a70358f7bc23311ca24b7fa
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-27 16:02:50 -0300

    feat(CategoryType): add showCategory method

--- src/model/CategoryType.java
+++ src/model/CategoryType.java
@@ -3,7 +3,7 @@ package model;
 public enum CategoryType {
     MOVIE("Filme"), SERIES("Série"), DOCUMENTARY("Documentário"), ANIMATION("Animação"), SHORT_FILM("Curta-metragem"), SHOW("Show"), REALITY_SHOW("Reality Show"), SPORTS("Esporte"), OTHERS("Outros");
 
-    private String description;
+    private final String description;
 
     CategoryType(String description) {
         this.description = description;
@@ -11,5 +11,11 @@ public enum CategoryType {
 
     public String getDescription() {
         return description;
+    }
+
+    public void showCategory() {
+        for (CategoryType category : CategoryType.values()) {
+            System.out.println(category.ordinal() + 1 + ". " + category.getDescription());
+        }
     }
 }
