dd875e955f96cf68a29a95d95faf9607e198f749
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-29 21:15:03 -0300

    feat(MenuService): add cases in interact method

--- src/service/MenuService.java
+++ src/service/MenuService.java
@@ -14,36 +14,85 @@ public class MenuService {
     }
 
     public boolean interact() {
-        boolean isOutsideRange;
+        boolean isRepeat;
+        MenuType chosenOption;
+        int toDo = 0;
+
+        MenuType.showMenu();
+        chosenOption = validateMenu();
+
+        switch (chosenOption) {
+            case ADDVIDEO -> {
+                toDo = 1;
+                isRepeat = true;
+            }
+            case EDITVIDEO -> {
+                toDo = 2;
+                isRepeat = true;
+            }
+            case LISTVIDEOS -> {
+                toDo = 3;
+                isRepeat = true;
+            }
+            case REMOVEVIDEO -> {
+                toDo = 4;
+                isRepeat = true;
+            }
+            case SORTVIDEOBYDATE -> {
+                toDo = 5;
+                isRepeat = true;
+            }
+            case SEARCHVIDEOSBYTITLE -> {
+                toDo = 6;
+                isRepeat = true;
+            }
+            case SHOWDSTATISTICREPORT -> {
+                toDo = 7;
+                isRepeat = true;
+            }
+            case FILTERVIDEOBYCATEGORY -> {
+                toDo = 8;
+                isRepeat = true;
+            }
+            case EXIT -> {
+                toDo = 9;
+                scanner.close();
+                isRepeat = false;
+            }
+            default -> isRepeat = false;
+        }
+
+        System.out.println(toDo);
+        return isRepeat;
+    }
+
+    private MenuType validateMenu() {
         boolean isNotValid = true;
+        int exitOrdinal;
         int chosenOption;
-        int exitOrdinal = MenuType.EXIT.ordinal() + 1;
 
         MenuType.showMenu();
 
+        exitOrdinal = MenuType.EXIT.ordinal() + 1;
         chosenOption = exitOrdinal;
         while (isNotValid) {
             System.out.print("Escolha uma opção entre 1 e " + rangeOfOptions + ": ");
-            if(scanner.hasNextInt()) {
+            if (scanner.hasNextInt()) {
                 chosenOption = scanner.nextInt();
-                if(chosenOption > 0 && chosenOption <= rangeOfOptions) {
+                if (chosenOption > 0 && chosenOption <= rangeOfOptions) {
                     isNotValid = false;
                 } else {
                     System.out.println("O número digitado está fora da faixa de valores.");
                 }
             } else {
                 System.out.println("A entrada digitada não é um número");
-                if(scanner.hasNextLine()){
+                if (scanner.hasNextLine()) {
                     scanner.next();
                 }
                 scanner.nextLine();
             }
         }
 
-        if (chosenOption == exitOrdinal){
-            scanner.close();
-            return false;
-        }
-        return true;
+        return MenuType.values()[chosenOption];
     }
 }
