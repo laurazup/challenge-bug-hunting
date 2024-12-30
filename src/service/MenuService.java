b461e6c54e286c055903a1f9fa0a0ab20f63f3a6
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-30 16:23:49 -0300

    refactor(Service): move validation methods from MenuService to ValidateService

--- src/service/MenuService.java
+++ src/service/MenuService.java
@@ -3,196 +3,73 @@ package service;
 import model.CategoryType;
 import model.MenuType;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
 public class MenuService {
-    private static Scanner scanner;
+    ValidateService validateService;
 
     public MenuService() {
-        scanner = new Scanner(System.in);
+        validateService = new ValidateService();
     }
 
     public boolean interactMenu() {
-        boolean isRepeat;
+        boolean isReInteractMenu;
         MenuType chosenOption;
         int toDo = 0;
 
         MenuType.showMenu();
-        chosenOption = validateMenu();
+        chosenOption = MenuType.values()[validateService.validateMenu(
+                MenuType.values().length,
+                MenuType.EXIT.ordinal())];
 
         switch (chosenOption) {
             case ADDVIDEO -> {
                 toDo = 1;
-                isRepeat = true;
+                CategoryType.showCategory();
+                // CategoryType value = CategoryType.values()[validateService.validateCategory(CategoryType.values().length)];
+                isReInteractMenu = true;
+                break;
             }
             case EDITVIDEO -> {
                 toDo = 2;
-                isRepeat = true;
+                CategoryType.showCategory();
+                // CategoryType value = CategoryType.values()[validateService.validateCategory(CategoryType.values().length)];
+                isReInteractMenu = true;
+                break;
             }
             case LISTVIDEOS -> {
                 toDo = 3;
-                isRepeat = true;
+                isReInteractMenu = true;
+                break;
             }
             case REMOVEVIDEO -> {
                 toDo = 4;
-                isRepeat = true;
+                isReInteractMenu = true;
+                break;
             }
             case SORTVIDEOBYDATE -> {
                 toDo = 5;
-                isRepeat = true;
+                isReInteractMenu = true;
             }
             case SEARCHVIDEOSBYTITLE -> {
                 toDo = 6;
-                isRepeat = true;
+                isReInteractMenu = true;
             }
             case SHOWDSTATISTICREPORT -> {
                 toDo = 7;
-                isRepeat = true;
+                isReInteractMenu = true;
             }
             case FILTERVIDEOBYCATEGORY -> {
                 toDo = 8;
-                isRepeat = true;
+                isReInteractMenu = true;
             }
             case EXIT -> {
                 toDo = 9;
-                scanner.close();
-                isRepeat = false;
+                validateService.close();
+                isReInteractMenu = false;
             }
-            default -> isRepeat = false;
+            default -> isReInteractMenu = false;
         }
 
         System.out.println(toDo);
-        return isRepeat;
-    }
-
-    private MenuType validateMenu() {
-        boolean isNotValid = true;
-        int rangeOfOptions = MenuType.values().length;
-        int exitOrdinal = MenuType.EXIT.ordinal();
-        int chosenOption;
-
-        MenuType.showMenu();
-
-        chosenOption = exitOrdinal;
-        while (isNotValid) {
-            System.out.print("Escolha uma opção entre 1 e " + rangeOfOptions + ": ");
-            if (scanner.hasNextInt()) {
-                chosenOption = scanner.nextInt();
-                if (chosenOption > 0 && chosenOption <= rangeOfOptions) {
-                    chosenOption--;
-                    isNotValid = false;
-                } else {
-                    System.out.println("O número digitado está fora da faixa de valores.");
-                }
-            } else {
-                System.out.println("A entrada digitada não é um número");
-                if (scanner.hasNextLine()) {
-                    scanner.next();
-                }
-                scanner.nextLine();
-            }
-        }
-
-        return MenuType.values()[chosenOption];
-    }
-
-    private String validateText() {
-        boolean isNotValid = true;
-        String inputUser = "";
-
-        while (isNotValid) {
-            inputUser = scanner.nextLine();
-            if (inputUser.isBlank()) {
-                System.err.println("O texto não pode ser vazia!");
-            } else if (inputUser.contains(";")) {
-                System.err.println("O carácter ';' não é permitido no texto!");
-            } else {
-                isNotValid = false;
-            }
-        }
-
-        return inputUser;
-    }
-
-    private int validateDurationInMinutes() {
-        boolean isNotValid = true;
-        int chosenOption = 1;
-
-        while (isNotValid) {
-            System.out.print("Digite a duração do vídeo (em minutos): ");
-            if (scanner.hasNextInt()) {
-                chosenOption = scanner.nextInt();
-                if (chosenOption > 0) {
-                    isNotValid = false;
-                } else {
-                    System.out.println("O número digitado não é maior que zero.");
-                }
-            } else {
-                System.out.println("A entrada digitada não é um número estritamente positivo");
-                if (scanner.hasNextLine()) {
-                    scanner.next();
-                }
-                scanner.nextLine();
-            }
-        }
-
-        return chosenOption;
-    }
-
-    private CategoryType validateCategory() {
-        boolean isNotValid = true;
-        int rangeOfOptions = CategoryType.values().length;
-        int chosenOption = 0;
-
-        CategoryType.showCategory();
-
-        while (isNotValid) {
-            System.out.print("Escolha uma categoria entre 1 e " + rangeOfOptions + ": ");
-            if (scanner.hasNextInt()) {
-                chosenOption = scanner.nextInt();
-                if (chosenOption > 0 && chosenOption <= rangeOfOptions) {
-                    chosenOption--;
-                    isNotValid = false;
-                } else {
-                    System.out.println("O número digitado está fora da faixa de valores.");
-                }
-            } else {
-                System.out.println("A entrada digitada não é um número");
-                if (scanner.hasNextLine()) {
-                    scanner.next();
-                }
-                scanner.nextLine();
-            }
-        }
-
-        return CategoryType.values()[chosenOption];
-    }
-
-    private Date validatePublicationDate() {
-        boolean isNotValid = true;
-        SimpleDateFormat simpleDateFormat;
-        Date publicationDate = null;
-
-        while (isNotValid) {
-            try {
-                simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-                simpleDateFormat.setLenient(false);
-
-                try {
-                    System.out.print("Digite a data de publicação no formato (dd/MM/yyyy): ");
-                    publicationDate = simpleDateFormat.parse(scanner.next());
-                    isNotValid = false;
-                } catch (ParseException e) {
-                    System.err.println("A data informada é inválida!");
-                }
-            } catch (NullPointerException | IllegalArgumentException e) {
-                System.err.println("Não foi possível criar o formatador de datas");
-            }
-        }
-
-        return publicationDate;
+        return isReInteractMenu;
     }
 }
--- src/service/ValidateService.java
+++ src/service/ValidateService.java
@@ -1,4 +1,138 @@
 package service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class ValidateService {
+    private static Scanner scanner;
+
+    public ValidateService() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int validateMenu(int rangeOfOptions, int exitOrdinal) {
+        boolean isNotValid = true;
+        int chosenOption = exitOrdinal;
+
+        while (isNotValid) {
+            System.out.print("Escolha uma opção entre 1 e " + rangeOfOptions + ": ");
+            if (scanner.hasNextInt()) {
+                chosenOption = scanner.nextInt();
+                if (chosenOption > 0 && chosenOption <= rangeOfOptions) {
+                    chosenOption--;
+                    isNotValid = false;
+                } else {
+                    System.out.println("O número digitado está fora da faixa de valores.");
+                }
+            } else {
+                System.out.println("A entrada digitada não é um número");
+                if (scanner.hasNextLine()) {
+                    scanner.next();
+                }
+                scanner.nextLine();
+            }
+        }
+
+        return chosenOption;
+    }
+
+    public String validateText() {
+        boolean isNotValid = true;
+        String inputUser = "";
+
+        while (isNotValid) {
+            inputUser = scanner.nextLine();
+            if (inputUser.isBlank()) {
+                System.err.println("O texto não pode ser vazia!");
+            } else if (inputUser.contains(";")) {
+                System.err.println("O carácter ';' não é permitido no texto!");
+            } else {
+                isNotValid = false;
+            }
+        }
+
+        return inputUser;
+    }
+
+    public int validateDurationInMinutes() {
+        boolean isNotValid = true;
+        int chosenOption = 1;
+
+        while (isNotValid) {
+            System.out.print("Digite a duração do vídeo (em minutos): ");
+            if (scanner.hasNextInt()) {
+                chosenOption = scanner.nextInt();
+                if (chosenOption > 0) {
+                    isNotValid = false;
+                } else {
+                    System.out.println("O número digitado não é maior que zero.");
+                }
+            } else {
+                System.out.println("A entrada digitada não é um número estritamente positivo");
+                if (scanner.hasNextLine()) {
+                    scanner.next();
+                }
+                scanner.nextLine();
+            }
+        }
+
+        return chosenOption;
+    }
+
+    public int validateCategory(int rangeOfOptions) {
+        boolean isNotValid = true;
+        int chosenOption = 0;
+
+        while (isNotValid) {
+            System.out.print("Escolha uma categoria entre 1 e " + rangeOfOptions + ": ");
+            if (scanner.hasNextInt()) {
+                chosenOption = scanner.nextInt();
+                if (chosenOption > 0 && chosenOption <= rangeOfOptions) {
+                    chosenOption--;
+                    isNotValid = false;
+                } else {
+                    System.out.println("O número digitado está fora da faixa de valores.");
+                }
+            } else {
+                System.out.println("A entrada digitada não é um número");
+                if (scanner.hasNextLine()) {
+                    scanner.next();
+                }
+                scanner.nextLine();
+            }
+        }
+
+        return chosenOption;
+    }
+
+    public Date validatePublicationDate() {
+        boolean isNotValid = true;
+        SimpleDateFormat simpleDateFormat;
+        Date publicationDate = null;
+
+        while (isNotValid) {
+            try {
+                simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                simpleDateFormat.setLenient(false);
+
+                try {
+                    System.out.print("Digite a data de publicação no formato (dd/MM/yyyy): ");
+                    publicationDate = simpleDateFormat.parse(scanner.next());
+                    isNotValid = false;
+                } catch (ParseException e) {
+                    System.err.println("A data informada é inválida!");
+                }
+            } catch (NullPointerException | IllegalArgumentException e) {
+                System.err.println("Não foi possível criar o formatador de datas");
+            }
+        }
+
+        return publicationDate;
+    }
+
+    public void close() {
+        scanner.close();
+    }
 }
