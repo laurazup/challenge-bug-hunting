4b15e4d0dc723c4b54a633ed6484487308bf3724
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-31 09:54:06 -0300

    feat(AttributeType): add attributes

--- src/model/AttributeType.java
+++ src/model/AttributeType.java
@@ -1,4 +1,10 @@
 package model;
 
 public enum AttributeType {
+    TITLE("Título"),
+    DESCRIPTION("Descrição"),
+    DURATIONINMINUTES("Duração em minutos"),
+    CATEGORY("Categoria"),
+    PUBLICATIONDATE("Data de publicação"),
+    EXIT("Sair");
 }
