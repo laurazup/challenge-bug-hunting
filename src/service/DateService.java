54323e648bb43a7c8486da2bf95fbe86732874bf
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-02 16:27:54 -0300

    feat(service): add constructor class

--- src/service/DateService.java
+++ src/service/DateService.java
@@ -1,4 +1,13 @@
 package service;
 
+import java.text.SimpleDateFormat;
+
 public class DateService {
+    SimpleDateFormat simpleDateFormat;
+
+    DateService() {
+        simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        simpleDateFormat.setLenient(false);
+    }
+
 }
