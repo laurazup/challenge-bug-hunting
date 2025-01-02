70e53c9aedaaed7fae89261edd0ab05fa2d141eb
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-02 14:12:20 -0300

    style(Main): add closing message

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,14 @@ import service.MenuService;
 
 public class Main {
     public static void main(String[] args) {
+        int quantityOfMenuInteraction = 0;
         MenuService menuService = new MenuService(".\\videos.csv");
 
         while (menuService.interactMenu()) {
-            continue;
+            quantityOfMenuInteraction++;
         }
+
+        System.out.println("Encerrando o Sistema de Gerenciamento de Vídeos após " +
+                quantityOfMenuInteraction + " interações com o menu principal...");
     }
 }
